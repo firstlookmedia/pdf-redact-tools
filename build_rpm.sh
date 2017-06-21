44dafbd43da4bb865b88392528f461a5a3519744
Micah Lee <micah@micahflee.com>
2017-06-21 14:36:22 -0700

    Change output of RPM build script

--- build_rpm.sh
+++ build_rpm.sh
@@ -10,5 +10,4 @@ python setup.py bdist_rpm --requires="ImageMagick, perl-Image-ExifTool"
 
 # install it
 echo ""
-echo "To install, run:"
-echo "sudo yum install dist/pdf-redact-tools-$VERSION-1.noarch.rpm"
+echo "RPM package: dist/pdf-redact-tools-$VERSION-1.noarch.rpm"
